f6c2e3fd81cace9c4c6860e31054f8082912029d
Ankit <4nkitd@gmail.com>
2021-02-26 00:41:25 +0530

    v2 new ui , firefox dev issue fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,56 +4,57 @@ $(function() {
     
     $('.tab_btn').on('click',(elem)=>{
         
-        $(elem.target).removeClass('active')
 
         var data_id = $(elem.target).data('id');
         $('.bg-tab-content').hide();
         $('#'+data_id).show();
 
-        $('#'+data_id).addClass('active');
+        $('.active_tab').removeClass('active_tab')
+
+        $(elem.target).addClass('active_tab');
 
     });
 
 
 
     $('.base64_encode').on('click',(elem)=>{
-        var dec_text = $('#base64_dec').val();        
-        $('#base64_enc').val(window.btoa(dec_text));
+        var dec_text = $('#base64_data').val();        
+        $('#base64_data').val(window.btoa(dec_text));
     })
 
     $('.base64_decode').on('click',(elem)=>{
-        var enc_text = $('#base64_enc').val();        
-        $('#base64_dec').val(window.atob(enc_text));
+        var enc_text = $('#base64_data').val();        
+        $('#base64_data').val(window.atob(enc_text));
     })
 
 
 
     $('.url_encode').on('click',(elem)=>{
-        var dec_text = $('#url_dec').val();        
-        $('#url_enc').val(window.encodeURI(dec_text));
+        var dec_text = $('#url_data').val();        
+        $('#url_data').val(window.encodeURI(dec_text));
     })
 
     $('.url_decode').on('click',(elem)=>{
-        var enc_text = $('#url_enc').val();        
-        $('#url_dec').val(window.decodeURI(enc_text));
+        var enc_text = $('#url_data').val();        
+        $('#url_data').val(window.decodeURI(enc_text));
     })
 
 
 
     $('.hex_encode').on('click',(elem)=>{
-        var dec_text = $('#hex_dec').val();        
-        $('#hex_enc').val(String2Hex(utf8.encode(dec_text)));
+        var dec_text = $('#hex_data').val();        
+        $('#hex_data').val(String2Hex(utf8.encode(dec_text)));
     })
 
     $('.hex_decode').on('click',(elem)=>{
-        var enc_text = $('#hex_enc').val();        
-        $('#hex_dec').val(hex2a(enc_text));
+        var enc_text = $('#hex_data').val();        
+        $('#hex_data').val(hex2a(enc_text));
     })
 
 
 
     $('.hash_encode').on('click',(elem)=>{
-        var dec_text = $('#hash_dec').val();  
+        var dec_text = $('#hash_data').val();  
         var enc_type = $('#hash_select_enc').val();
         var hash_salt = $('#hash_salt').val();
 
@@ -62,7 +63,7 @@ $(function() {
             return false;
         }
 
-        let enc_res = hash_salt+'.';
+        let enc_res = '';
         
         if(enc_type == 'MD5'){
             enc_res += CryptoJS.MD5(dec_text);
@@ -75,13 +76,16 @@ $(function() {
         } else if (enc_type == 'AES'){
             enc_res = CryptoJS.AES.encrypt(dec_text,hash_salt);
         }
+        if (hash_salt){
+            enc_res = hash_salt + enc_res;
+        }
 
 
-        $('#hash_enc').val(enc_res);
+        $('#hash_data').val(enc_res);
     })
 
     $('.hash_decode').on('click',(elem)=>{
-        var enc_text = $('#hash_enc').val();  
+        var enc_text = $('#hash_data').val();  
         var enc_type = $('#hash_select_enc').val();
         var hash_salt = $('#hash_salt').val();  
         
@@ -95,7 +99,7 @@ $(function() {
         }
 
         $('.aes-alert').html('If text is not readable try hex decoding');
-        $('#hash_dec').val(enc_res);
+        $('#hash_data').val(enc_res);
     })
 
 });

c3b922ec9f24d0b177e474ce95cd87111e81475f
Ankit <4nkitd@gmail.com>
2021-03-11 01:54:43 +0530

    added auto form filler inital code

---
+++ injection/injection.js
@@ -0,0 +1,185 @@
+console.log('%c h4cker Injected ', 'color: #fff; font-size: 40px; background-image: linear-gradient(to right, #d13939, #cd5a16, #be7900, #a49500, #80ad08, #5eaa33, #3ba64d, #00a062, #00825d, #006552, #0e4842, #1a2d2c); border: solid 1px #fff; border-radius: 28px; margin: 10px;');
+
+var svg_1 = `<?xml version="1.0" encoding="iso-8859-1"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd"> <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="24" height="24" viewBox="0 0 919.8 919.8" style="enable-background:new 0 0 919.8 919.8;" xml:space="preserve" > <g> <path d="M684.833,209.4l27-33.8c37-46.2,29.5-113.7-16.801-150.7l-1.699-1.4c-46.2-37-113.7-29.5-150.7,16.8l-26.9,33.7 L684.833,209.4z"/> <path d="M520.532,493.5c0.9,8.199,5,16,11.9,21.6c13.9,11.1,34.2,8.9,45.3-5l185.5-232c11.3-14.1,8.8-34.8-5.7-45.8 c-14-10.6-34.1-7.6-45.1,6.1l-18.1,22.6l-31-24.8l0.1-0.1l-169-135.5l-432.4,540.5l169.1,135.4l392.1-490l31,24.8l-126.8,158.5 C521.833,476.699,519.633,485.3,520.532,493.5z"/> <path d="M203.032,801.5l-159.3-127.601c-2.6,8.9-3.9,16.7-3.9,16.7l-36.5,174.8c-2.6,12.601,10.2,22.9,21.9,17.5l162.6-74 C187.732,808.899,194.932,805.899,203.032,801.5z"/> <path d="M893.633,832.8c-1.4,0-2.7,0.1-4.101,0.3l-245.699,31.8c-12.601,1.601-21.4-12.2-14.601-23c3.7-5.7,7.3-11.5,10.9-17.3 c9.5-15.4-3.2-35.9-20.8-35.9c-1.2,0-2.4,0.101-3.7,0.301l-322,48.399l-38.5,5.8l-105,15.801c-11.9,1.8-19.9,11.899-18.9,24 c0.4,5.399,2.6,10.5,6.1,14.3c4.1,4.5,9.8,7,16,7c1.2,0,2.4-0.101,3.7-0.3l383.801-57.7c13.1-2,22.1,12.8,14.399,23.6 c-2.5,3.4-5,6.9-7.5,10.3c-6.899,9.4-5.3,25.101,2.7,33.2c4.1,4.2,9.3,6.4,14.8,6.4c0.601,0,1.3,0,1.9-0.101h0.1h0.101 l331.3-42.199h0.1h0.101c5.699-0.9,10.699-4.2,14-9.301c3.6-5.6,4.899-12.5,3.3-18.699 C913.532,839.399,904.732,832.8,893.633,832.8z"/> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>`;
+
+var svg_2 = `<svg height="24" viewBox="0 0 512 512" width="24" xmlns="http://www.w3.org/2000/svg"><g id="Outline"><path d="m488 128h-24v-8a8 8 0 0 0 -8-8h-77.333l-29.867-22.4a8 8 0 0 0 -4.8-1.6h-14.459l1.43-.572a8 8 0 0 0 5.029-7.428 48.014 48.014 0 0 0 -48-48h-56v16h56a32.036 32.036 0 0 1 31.574 26.754l-33.115 13.246h-53.459a25.013 25.013 0 0 0 -21.9 12.954l-35.1-25.071v-27.883h40v-16h-80a8 8 0 0 0 -8 8v40a8 8 0 0 0 8 8h8a24 24 0 0 0 0 48h16v32a8 8 0 0 0 8 8h1.376a23.943 23.943 0 0 0 4.755 24 23.943 23.943 0 0 0 0 32 23.956 23.956 0 0 0 3.807 35.431 319.47 319.47 0 0 0 -12.487 34.269l15.324 4.6a303.329 303.329 0 0 1 12.612-34.3h38.613a23.984 23.984 0 0 0 17.869-40 23.943 23.943 0 0 0 0-32 23.943 23.943 0 0 0 0-32h31.885l6.225 24.89a318.39 318.39 0 0 0 24.105 65.156 303.15 303.15 0 0 1 23.831 65.754 302.5 302.5 0 0 1 8.085 69.46v70.74a16.019 16.019 0 0 1 -16 16h-144a16.019 16.019 0 0 1 -16-16v-70.74a302.857 302.857 0 0 1 6.76-63.578l-15.643-3.364a318.968 318.968 0 0 0 -7.117 66.942v70.74a32.036 32.036 0 0 0 32 32h144a32.036 32.036 0 0 0 32-32v-70.74a318.542 318.542 0 0 0 -8.514-73.139 319.185 319.185 0 0 0 -25.091-69.23 302.356 302.356 0 0 1 -22.895-61.882l-3.923-15.689a66.982 66.982 0 0 0 36.423 10.68v-16a50.837 50.837 0 0 1 -47.428-32.111l-1.144-2.861a8 8 0 0 0 -6.436-4.966l-49.112-6.14a9 9 0 0 1 1.12-17.922h108.333l29.867 22.4a8 8 0 0 0 4.8 1.6h72v112h-16v-88h-16v88h-72v16h112a8 8 0 0 0 8-8v-8h24a8 8 0 0 0 8-8v-96a8 8 0 0 0 -8-8zm-283.02-4.362a23.812 23.812 0 0 0 2.289-5.759l1.471 1.051a25.052 25.052 0 0 0 7.26 12.365v27.72l-.246.985h-14.816zm-60.98-75.638h16v24h-16zm-8 64a8.009 8.009 0 0 1 8-8h40a8 8 0 0 1 0 16h-40a8.009 8.009 0 0 1 -8-8zm51.536 23.738-2.696 24.262h-8.84v-24s10.382-.091 11.536-.262zm44.464 40.262a8 8 0 0 1 0 16h-40a8 8 0 0 1 0-16zm8 40a8.009 8.009 0 0 1 -8 8h-40a8 8 0 0 1 0-16h40a8.009 8.009 0 0 1 8 8zm-8 40h-40a8 8 0 0 1 0-16h40a8 8 0 0 1 0 16zm46.807-104h-46.807v-13.938l42.348 5.293a67.826 67.826 0 0 0 4.459 8.645zm201.193 72h-16v-80h16z"/><path d="m72 72h32v16h-32z"/><path d="m16 72h32v16h-32z"/><path d="m83.022 24.134h16v32h-16z" transform="matrix(.446 -.895 .895 .446 14.484 103.676)"/><path d="m32.908-.859h16v32h-16z" transform="matrix(.446 -.895 .895 .446 9.102 44.992)"/><path d="m75.022 111.141h32v16h-32z" transform="matrix(.895 -.446 .446 .895 -43.604 53.145)"/><path d="m24.908 136.133h32v16h-32z" transform="matrix(.895 -.446 .446 .895 -60.025 33.407)"/><path d="m264 416a56.064 56.064 0 0 0 56-56v-32a8 8 0 0 0 -8-8h-64a56.064 56.064 0 0 0 -56 56v32a8 8 0 0 0 8 8zm-56-40a40.045 40.045 0 0 1 40-40h56v24a40.045 40.045 0 0 1 -40 40h-56z"/></g></svg>`;
+
+var svg_3 = `<?xml version="1.0" encoding="iso-8859-1"?><svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"  height="24" width="24" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"> <g> <g> <path d="M507.353,245.245l-83.692-78.769c-4.289-4.039-10.57-5.141-15.98-2.803c-5.409,2.337-8.911,7.666-8.911,13.558v34.462 h-98.462v-98.462h34.462c5.893,0,11.221-3.502,13.558-8.911c2.336-5.409,1.236-11.69-2.803-15.98L266.755,4.647 C263.964,1.682,260.072,0,256,0c-4.072,0-7.964,1.682-10.755,4.647L166.476,88.34c-4.039,4.29-5.141,10.571-2.803,15.98 c2.337,5.409,7.666,8.911,13.558,8.911h34.462v98.462h-98.462v-34.462c0-5.893-3.502-11.221-8.911-13.558 c-5.41-2.337-11.69-1.236-15.98,2.803L4.647,245.245C1.682,248.036,0,251.928,0,256s1.682,7.964,4.647,10.755l83.692,78.769 c4.29,4.039,10.57,5.142,15.98,2.803c5.409-2.337,8.911-7.666,8.911-13.558v-34.462h98.462v98.462h-34.462 c-5.893,0-11.221,3.502-13.558,8.911c-2.336,5.41-1.236,11.69,2.803,15.98l78.769,83.692C248.036,510.318,251.928,512,256,512 c4.072,0,7.964-1.682,10.755-4.647l78.769-83.692c4.039-4.29,5.141-10.571,2.803-15.98c-2.337-5.409-7.666-8.911-13.558-8.911 h-34.462v-98.462h98.462v34.462c0,5.893,3.502,11.221,8.911,13.558c5.411,2.337,11.691,1.236,15.98-2.803l83.692-78.769 C510.318,263.964,512,260.072,512,256C512,251.928,510.318,248.035,507.353,245.245z M428.308,300.587v-15.049 c0-8.157-6.613-14.769-14.769-14.769h-128c-8.157,0-14.769,6.613-14.769,14.769v128c0,8.157,6.613,14.769,14.769,14.769h15.049 L256,475.682l-44.587-47.374h15.049c8.157,0,14.769-6.613,14.769-14.769v-128c0-8.157-6.613-14.769-14.769-14.769h-128 c-8.157,0-14.769,6.613-14.769,14.769v15.049L36.319,256l47.374-44.587v15.049c0,8.157,6.613,14.769,14.769,14.769h128 c8.157,0,14.769-6.613,14.769-14.769v-128c0-8.157-6.613-14.769-14.769-14.769h-15.049L256,36.319l44.587,47.374h-15.049 c-8.157,0-14.769,6.613-14.769,14.769v128c0,8.157,6.613,14.769,14.769,14.769h128c8.157,0,14.769-6.613,14.769-14.769v-15.049 L475.682,256L428.308,300.587z"/> </g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> <g> </g> </svg>`;
+
+var toolbar_html = `
+
+  <div class="h4c3r_toolbar">
+    <div class="h4c3r_toolbar_main" id="toolbar_id_">
+      <div class="h4c3r_toolbar_item h4c3r_fill">
+        <span>
+            ${svg_1}
+        </span>
+      </div>
+      <div class="h4c3r_toolbar_item ">
+        <span class="h4c3r_reset" >
+            ${svg_2}
+        </span>
+      </div>
+      <div class="h4c3r_toolbar_item " id="header" >
+        <span class="h4c3r_drag" >
+            ${svg_3}
+        </span>
+      </div>
+    </div>
+  </div>
+  </div>
+  
+  `;
+
+$('body').prepend(toolbar_html);
+
+
+$('.h4c3r_fill').on('click',(e) => {
+    fill_email();
+    fill_text();
+    fill_tel();
+    fill_text_area();
+})
+
+$('.h4c3r_reset').on('click', (e) => {
+
+     $("input").val('');
+     $("textarea").val('');
+
+})
+
+
+const fill_email = function () {
+   
+    var sample_email = random_string() + '@yopmail.com';
+    
+    $("input[type='text']").each((i) => {
+
+        var elm = $("input[type='email']")[i];
+
+        $(elm).val(sample_email);
+        $(elm).trigger('change');
+
+    })
+
+}
+
+const fill_text = function () {
+
+     $("input[type='text']").each((i)=>{
+    
+        var elm = $("input[type='text']")[i];
+        var str_len  = 10;
+
+         var max_length = $(elm).attr('maxlength');
+         
+         var minlength = $(elm).attr('minlength');
+
+         if (minlength){
+             str_len = minlength + 1;
+         }
+
+         if (max_length) {
+             str_len = max_length - 1;
+         }
+
+         var sample_string = random_string(str_len);
+    
+        $(elm).val(sample_string);
+        $(elm).trigger('change');
+     
+    })
+     
+}
+
+const fill_tel = function () {
+
+    $("input[type='tel']").each((i) => {
+
+        var elm = $("input[type='tel']")[i];
+
+        var sample_string = '9' + random_number(9);
+
+        $(elm).val(sample_string);
+        $(elm).trigger('change');
+
+    })
+
+}
+
+const fill_number = function () {
+
+    $("input[type='number']").each((i) => {
+
+        var elm = $("input[type='number']")[i];
+
+        var str_len = 3;
+
+        var max_length = $(elm).attr('min').length;
+
+        var minlength = $(elm).attr('max').length;
+
+        if (minlength) {
+            str_len = minlength;
+        }
+
+        if (max_length) {
+            str_len = max_length;
+        }
+
+        var sample_string = random_number(str_len);
+
+        $(elm).val(sample_string);
+        $(elm).trigger('change');
+
+    })
+
+}
+
+const fill_text_area = function () {
+
+    $("textarea").each((i) => {
+
+        var elm = $("textarea")[i];
+        var str_len = 50;
+
+        var max_length = $(elm).attr('maxlength');
+
+        var minlength = $(elm).attr('minlength');
+
+        if (minlength) {
+            str_len = minlength + 1;
+        }
+
+        if (max_length) {
+            str_len = max_length - 1;
+        }
+
+        var sample_string = random_string(str_len);
+
+        $(elm).html(sample_string);
+        $(elm).trigger('change');
+
+    })
+
+}
+
+function random_string(length = 8) {
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+function random_number(length = 8) {
+    var result = '';
+    var characters = '0123456789';
+    var charactersLength = characters.length;
+    for (var i = 0; i < length; i++) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}

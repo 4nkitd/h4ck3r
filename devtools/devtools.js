51a4a4ca37bccb89cf05fc7c3612b7c130b8db53
4nkitd <4nkitd@gmail.com>
2021-10-30 16:06:00 +0530

    base compoenetsa ready

--- devtools/devtools.js
+++ devtools/devtools.js
@@ -1,10 +1,10 @@
 navigator.userAgent.indexOf('Firefox') != -1 &&
-	browser.devtools.panels.create('h4ck3r', '../assets/icons/icon.png', '../index.html');
+    browser.devtools.panels.create('h4ck3r', '/assets/icons/icon.png', '../main/main.html');
 
 navigator.userAgent.indexOf('Chrome') != -1 &&
-	chrome.devtools.panels.create(
-		'h4ck3r', // title for the panel tab
-		"../assets/icons/icon.png", //   path to an icon
-		'../index.html', // html page which is gonna be injecting into the tab's content
-		null // callback function here
-	);
+    chrome.devtools.panels.create(
+        'h4ck3r', // title for the panel tab
+        "/assets/icons/icon.png", //   path to an icon
+        '../main/main.html', // html page which is gonna be injecting into the tab's content
+        null // callback function here
+    );

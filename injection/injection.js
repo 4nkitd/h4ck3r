a36171c55be371d5da84edb418f19c9ffe4e1fee
4nkitd <4nkitd@gmail.com>
2021-11-01 01:37:13 +0530

    input type detection

--- injection/injection.js
+++ injection/injection.js
@@ -23,3 +23,82 @@ function fillForm() {
         randomCard,
     })
 }
+
+setTimeout(() => {
+    fillForm()
+})
+
+function input() {
+    $("input").each(() => {
+        const type = $(this).attr('type');
+
+        switch (type) {
+            case 'button':
+                break;
+            case 'checkbox':
+                $(this).val(true);
+                break;
+            case 'color':
+                $(this).val(faker.internet.color());
+                break;
+            case 'date':
+                $(this).val(faker.date.past());
+                break;
+            case 'datetime-local':
+                $(this).val(faker.date.recent())
+                break;
+            case 'email':
+                $(this).val(getEmail());
+                break;
+            case 'file':
+                break;
+            case 'hidden':
+                break;
+            case 'image':
+                break;
+            case 'month':
+                $(this).val(faker.date.month());
+                break;
+            case 'number':
+                $(this).val(faker.number.random());
+                break;
+            case 'password':
+                $(this).val('#Pass@123');
+                break;
+            case 'radio':
+                $(this).prop("checked", true);
+                break;
+            case 'range':
+                break;
+            case 'reset':
+                break;
+            case 'search':
+                $(this).val(faker.lorem.word());
+                break;
+            case 'submit':
+                break;
+            case 'tel':
+                $(this).val(faker.phone.phoneNumber());
+                break;
+            case 'text':
+                $(this).val(faker.lorem.word());
+                break;
+            case 'time':
+                $(this).val(faker.time.recent());
+                break;
+            case 'url':
+                $(this).val(faker.internet.url());
+                break;
+            case 'week':
+                $(this).val(faker.date.weekday());
+                break;
+            default:
+                break;
+        }
+
+    });
+}
+
+function getEmail() {
+    return faker.lorem.slug() + "@yopmail.com";
+}
